027277d1304da0b3ebbcbae0e507b65eeaa8f341
mutiashl <mutiashl76@gmail.com>
2024-07-05 12:55:57 +0700

    versi 1.0.0

---
+++ application/views/home/index.php
@@ -0,0 +1,23 @@
+<!-- Carousel -->
+<div id="carouselExampleFade" class="carousel slide carousel-fade bg-light" data-ride="carousel">
+  <div class="container">
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <div class="row d-flex justify-content-center">
+          <div class="col-lg-5">
+            <h4>Selamat datang di Goho Coffee <br>tempat untuk bersantai <br>dan menenangkan pikiran</h4>
+            <p>tempat di mana cita rasa dan kenyamanan bertemu dalam harmoni yang sempurna. Terletak di jantung kota, Goho Coffee menawarkan suasana hangat dan ramah yang ideal untuk bersantai, berkumpul dengan teman, atau sekadar menikmati waktu sendirian dengan secangkir kopi yang nikmat.</p>
+            <a href="<?= base_url('menu'); ?>" class="btn btn-order">Order Now <i class="fas fa-arrow-right ml-3"></i></a>
+          </div>
+          <div class="col-lg-5 d-flex justify-content-end">
+            <div class="img-bg">
+              <img src="<?= base_url('assets/img/GAMBAR.png'); ?>" width="400px" height="430")
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- End Carousel -->
+
